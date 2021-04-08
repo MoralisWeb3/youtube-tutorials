67f4a0fed96329e95f61a95f97f335cd3abc49c8
Jeremy May <may.jeremy.r@gmail.com>
2021-04-08 08:40:49 -0700

    moralis scan EP 01 - env setup

---
+++ moralis-scan/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Moralis Scan</h1>
+    </div>
+  );
+}
+
+export default App;
