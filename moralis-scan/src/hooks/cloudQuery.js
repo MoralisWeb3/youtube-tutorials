e458dd72c26b7fed7b8adad5ccf7d16df07d63c8
Jeremy May <may.jeremy.r@gmail.com>
2021-04-08 20:32:01 -0700

    cloning etherscan - part 2

---
+++ moralis-scan/src/hooks/cloudQuery.js
@@ -0,0 +1,41 @@
+import Moralis from "moralis";
+import { useEffect, useState } from "react";
+
+const defaultCloudQueryOptions = {
+  params: {},
+  postProcess: (r) => r.attributes,
+};
+
+export function useMoralisCloudQuery(
+  methodName,
+  options = defaultCloudQueryOptions
+) {
+  const [state, setState] = useState({
+    data: null,
+    error: null,
+    loading: false,
+  });
+
+  useEffect(() => {
+    if (methodName) {
+      setState((v) => ({ ...v, loading: true }));
+      Moralis.Cloud.run(methodName, options.params)
+        .then((data) => {
+          if (data) {
+            const output = options.postProcess
+              ? data.map(options.postProcess)
+              : data;
+            setState({ data: output, error: null, loading: false });
+          } else {
+            setState({ data: null, error: null, loading: false });
+          }
+        })
+        .catch((error) => {
+          console.error(error);
+          setState({ data: null, error, loading: false });
+        });
+    }
+  }, [methodName, options]);
+
+  return state;
+}
