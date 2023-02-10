35b8df4b65db1887d638d23df5dddd06171f2038
Jossif Elefteriadis <jossif_@hotmail.com>
2023-02-10 15:45:49 +0100

    Add code for fetching aptos nft by txs hash

---
+++ get-aptos-nft-transaction-by-hash/index.js
@@ -0,0 +1,18 @@
+import fetch from "node-fetch";
+
+const options = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    "X-API-Key":
+      "q2uxXuKPvqa57IlGGFJffeBZx51iFlPvFidpSvcqYI31jwbukVgc2P0v7CIAB1rO",
+  },
+};
+
+fetch(
+  "https://testnet-aptos-api.moralis.io/transactions/by_hash/0x3f3a083fbba0a6c1aa7f21664cdc5041666d86ca7b5e620e0130678697149359",
+  options
+)
+  .then((response) => response.json())
+  .then((response) => console.log(response))
+  .catch((err) => console.error(err));
